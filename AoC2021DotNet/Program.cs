69452ac0f98d2753612733463dd0187d16d873f2
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2021-12-02 08:31:37 +0100

    Day 1 & 2 solutions

---
+++ AoC2021DotNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AoC2021DotNet.AoC;
+
+namespace AoC2021DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day2().Part2();
+        }
+    }
+}
