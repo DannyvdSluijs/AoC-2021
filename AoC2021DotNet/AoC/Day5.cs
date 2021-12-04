87e5d1aed6cc51ceaee4268f47c753119e6bd603
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2021-12-04 10:50:34 +0100

    Add .gitignore

---
+++ AoC2021DotNet/AoC/Day5.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2021DotNet.AoC
+{
+    public class Day4
+    {
+        public void Part1()
+        {
+            var data = input.Trim()
+                .Split("\n")
+
+                .ToList();
+
+
+            Console.WriteLine($"");
+        }
+
+        public void Part2()
+        {
+            var data = input.Trim()
+                .Split("\n")
+
+                .ToList();
+
+
+            Console.WriteLine($"");
+        }
+
+        private string testData = @"
+";
+
+        private string input = @"
+";
+    }
+}
